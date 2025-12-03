c658f386d52538c7c1dd7e7ad518df540973a723
VillanuevaMateo <mateovillanueva@uca.edu.ar>
2025-12-03 16:15:30 -0300

    Se emprolija el codigo de paquete y se hace enfasis en la privatizacion de las caracteristicas y validaciones.

--- src/paquete.js
+++ src/paquete.js
@@ -1,5 +1,5 @@
 const Paquete = function (gb, minutos, duracionDias, costo) {
-  // Variables privadas
+  // --- Variables privadas ---
   let _gbIniciales = gb;
   let _minutosIniciales = minutos;
   let _gb = gb;
@@ -8,39 +8,55 @@ const Paquete = function (gb, minutos, duracionDias, costo) {
   let _costo = costo;
   let _fechaCompra = new Date();
 
-  // Funciones privadas de validación
+  // --- Validaciones privadas ---
   const validarCantidadPositiva = (cantidad) => {
-    if (cantidad <= 0) throw new Error("La cantidad a consumir debe ser positiva");
+    if (cantidad <= 0) {
+      throw new Error("La cantidad a consumir debe ser positiva");
+    }
   };
 
   const validarGBDisponibles = (cantidad) => {
-    if (cantidad > _gb) throw new Error("No hay suficientes GB disponibles");
+    if (cantidad > _gb) {
+      throw new Error("No hay suficientes GB disponibles");
+    }
   };
 
   const validarMinutosDisponibles = (cantidad) => {
-    if (cantidad > _minutos) throw new Error("No hay suficientes minutos disponibles");
+    if (cantidad > _minutos) {
+      throw new Error("No hay suficientes minutos disponibles");
+    }
   };
 
-  // Métodos públicos
+  // --- Funciones privadas de consumo ---
+  const consumirInternet = (cantidad) => {
+    validarGBDisponibles(cantidad);
+    _gb -= cantidad;
+    return true;
+  };
+
+  const consumirMinutos = (cantidad) => {
+    validarMinutosDisponibles(cantidad);
+    _minutos -= cantidad;
+    return true;
+  };
+
+  // --- Método público principal ---
   this.descontarConsumo = function (tipo, cantidad) {
     validarCantidadPositiva(cantidad);
 
-    if (tipo === "internet") {
-      validarGBDisponibles(cantidad);
-      _gb -= cantidad;
-      return true;
-    }
+    switch (tipo) {
+      case "internet":
+        return consumirInternet(cantidad);
 
-    if (tipo === "llamadas") {
-      validarMinutosDisponibles(cantidad);
-      _minutos -= cantidad;
-      return true;
-    }
+      case "llamadas":
+        return consumirMinutos(cantidad);
 
-    throw new Error("Tipo de consumo desconocido");
+      default:
+        throw new Error("Tipo de consumo desconocido");
+    }
   };
 
-  // Getters
+  // --- Getters públicos ---
   this.gb = () => _gb;
   this.minutos = () => _minutos;
   this.gbIniciales = () => _gbIniciales;
@@ -48,6 +64,7 @@ const Paquete = function (gb, minutos, duracionDias, costo) {
   this.duracionDias = () => _duracionDias;
   this.costo = () => _costo;
 
+  // --- Estados ---
   this.estaAgotado = () => _gb <= 0 && _minutos <= 0;
 
   this.estaVencido = (fechaActual = new Date()) => {
