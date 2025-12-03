0c60b731364b279fc63a7b5ab1ce829cd949620c
VillanuevaMateo <mateovillanueva@uca.edu.ar>
2025-12-03 11:50:35 -0300

    Agregar test desde cliente

--- test/cliente.test.js
+++ test/cliente.test.js
@@ -1,10 +1,16 @@
 "use strict";
 const Cliente = require('../src/Cliente');
+const CuentaPrepago = require('../src/cuentaPrepago');
 
 describe('Cliente', () => {
     test('debería crearse con nombre completo y número de línea', () => {
         const cliente = new Cliente('Mateo Villanueva', '1155555555');
         expect(cliente.nombreCompleto).toBe('Mateo Villanueva');
         expect(cliente.numeroLinea).toBe('1155555555');
+    });
+    test('debería crearse con una cuenta bancaria con saldo inicial', () => {
+        const cliente = new Cliente('Mateo Villanueva', '1155555555', 500);
+
+        expect(cliente.cuentaPrepago.saldo).toBe(500);
     });
 });
