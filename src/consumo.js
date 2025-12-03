6d99db65454946065251444a4ace080fc388a516
VillanuevaMateo <mateovillanueva@uca.edu.ar>
2025-12-03 15:03:22 -0300

    Hago refactor para prototipar mejor los consumos y mantener sus atributos privados

--- src/consumo.js
+++ src/consumo.js
@@ -1,35 +1,39 @@
-// src/consumo.js
-const Consumo = function(tipo, cantidad, fechaInicio, fechaFin) {
+const Consumo = function (tipo, cantidad, fechaInicio, fechaFin) {
   if (tipo !== "internet" && tipo !== "llamadas") {
     throw new Error("Tipo de consumo desconocido");
   }
-
   if (cantidad <= 0) {
     throw new Error("La cantidad a consumir debe ser positiva");
   }
 
-  // Propiedades privadas
-  this._tipo = tipo;
-  this._cantidad = cantidad;
-  this._fechaInicio = fechaInicio;
-  this._fechaFin = fechaFin;
+  // Propiedades privadas usando closure
+  let _tipo = tipo;
+  let _cantidad = cantidad;
+  let _fechaInicio = fechaInicio;
+  let _fechaFin = fechaFin;
+
+  // Getters privados para usar desde prototype
+  this._getTipo = () => _tipo;
+  this._getCantidad = () => _cantidad;
+  this._getFechaInicio = () => _fechaInicio;
+  this._getFechaFin = () => _fechaFin;
 };
 
-// Métodos públicos
-Consumo.prototype.getTipo = function() {
-  return this._tipo;
+// Métodos públicos en el prototype
+Consumo.prototype.getTipo = function () {
+  return this._getTipo();
 };
 
-Consumo.prototype.getCantidad = function() {
-  return this._cantidad;
+Consumo.prototype.getCantidad = function () {
+  return this._getCantidad();
 };
 
-Consumo.prototype.getFechaInicio = function() {
-  return this._fechaInicio;
+Consumo.prototype.getFechaInicio = function () {
+  return this._getFechaInicio();
 };
 
-Consumo.prototype.getFechaFin = function() {
-  return this._fechaFin;
+Consumo.prototype.getFechaFin = function () {
+  return this._getFechaFin();
 };
 
 module.exports = Consumo;
